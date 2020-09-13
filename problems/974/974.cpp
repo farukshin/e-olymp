63050d56644e34a0f94002728432b80a8bc29a6a
Amir Farukshin <farukshin.amir@gmail.com>
2020-09-13 18:46:26 +0300

    docs(): решил задачи Флойд и Флойд - 1

---
+++ problems/974/974.cpp
@@ -0,0 +1,55 @@
+//https://www.e-olymp.com/ru/problems/974
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void Floyd_Warshall(vector<vector<int>>& ms, int& countNode)
+{
+    for (int k = 0; k < countNode; ++k)
+        for (int i = 0; i < countNode; ++i)
+            for (int j = 0; j < countNode; ++j)
+                if (ms[i][k] < INT_MAX && ms[k][j] < INT_MAX)
+                    ms[i][j] = min(ms[i][j], ms[i][k] + ms[k][j]);
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<vector<int>> ms(n);
+    for (int i = 0;i < n;i++)
+        for (int j = 0;j < n;j++)
+        {
+            int cur;
+            cin >> cur;
+            ms[i].push_back(cur);
+        }
+
+
+    Floyd_Warshall(ms, n);
+
+    for (int i = 0;i < n;i++)
+    {
+        for (int j = 0;j < n;j++)
+            cout << ms[i][j] << " ";
+        cout << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
