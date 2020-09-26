9058fc783c887fe4573998f5d5edeeaf020afa6e
Amir Farukshin <farukshin.amir@gmail.com>
2020-09-26 21:23:50 +0300

    docs(): решил задачи 933 935 939 940

---
+++ problems/933/933.cpp
@@ -0,0 +1,36 @@
+//https://www.e-olymp.com/ru/problems/933
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    cout.setf(ios::fixed);
+    cout.precision(0);
+
+    string s;
+    cin >> s;
+    ll ans = 0;
+    for (auto ch : s)
+        if (ch >= '0' && ch <= '9')
+            ans += ch - '0';
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
