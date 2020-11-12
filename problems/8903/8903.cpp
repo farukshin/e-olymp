6f659d5178a6e52032d492fca673df8d238efdf3
Amir Farukshin <farukshin.amir@gmail.com>
2020-11-12 18:32:00 +0300

    docs(): зафиксировал решение задачи 8903

---
+++ problems/8903/8903.cpp
@@ -0,0 +1,32 @@
+//https://www.e-olymp.com/ru/problems/8903
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    n--;
+    while (n % 5 != 0)
+        n--;
+
+    cout << n << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
