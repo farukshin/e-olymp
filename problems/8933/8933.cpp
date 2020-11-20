e0af1f4afefad10829ada44ded06f236bfcad21d
Amir Farukshin <farukshin.amir@gmail.com>
2020-11-20 23:03:00 +0300

    docs(): зафиксировал решение задач 8933 8934

---
+++ problems/8933/8933.cpp
@@ -0,0 +1,33 @@
+//https://www.e-olymp.com/ru/problems/8933
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    // cout.setf(std::ios::fixed);
+    // cout.precision(4);
+
+    int a;
+    cin >> a;
+
+    for (int i = a; i >= 0; i--)
+        cout << i << " sek" << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
