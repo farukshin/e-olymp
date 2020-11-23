1daec492f5c1c3436c2fda0c345232cc5a468b84
Amir Farukshin <farukshin.amir@gmail.com>
2020-11-23 20:08:00 +0300

    docs(): зафиксировал решение задачи 8953

---
+++ problems/8953/8953.cpp
@@ -0,0 +1,38 @@
+//https://www.e-olymp.com/ru/problems/8953
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    // cout.setf(std::ios::fixed);
+    // cout.precision(4);
+
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int cur;
+        cin >> cur;
+        cout << cur << endl;
+    }
+
+    //cout << ((a * b) % 2 == 0 ? 0 : 1) << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
