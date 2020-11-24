98c69deaf6419fb5db87bdbc2396d4947eac8c83
Amir Farukshin <farukshin.amir@gmail.com>
2020-11-24 22:13:00 +0300

    docs(): решил задачи 8955 8956 8957

---
+++ problems/8955/8955.cpp
@@ -0,0 +1,50 @@
+//https://www.e-olymp.com/ru/problems/8955
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    // cout.setf(std::ios::fixed);
+    // cout.precision(4);
+
+    int n;
+    cin >> n;
+    int arr[n], k = 0;
+    bool succes = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] > 0)
+            k++, succes = true;
+    }
+    if (!succes)
+        cout << "NO" << endl;
+    else
+    {
+        cout << k << endl;
+        for (int i = 0; i < n; i++)
+            if (arr[i] > 0)
+                cout << arr[i] << ' ';
+    }
+
+    //cout << ((a * b) % 2 == 0 ? 0 : 1) << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
