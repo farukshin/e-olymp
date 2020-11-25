77494ddcfe8b298e2b7f401d8f58ff44b949cb28
Amir Farukshin <farukshin.amir@gmail.com>
2020-11-25 19:16:00 +0300

    docs(): 9000 9026

---
+++ problems/9000/9000.cpp
@@ -0,0 +1,39 @@
+//https://www.e-olymp.com/ru/problems/9000
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    // cout.setf(std::ios::fixed);
+    // cout.precision(4);
+
+    map<char, int> maps;
+    string s;
+    getline(cin, s);
+
+    for (auto ch : s)
+        if (ch >= 'a' && ch <= 'z')
+            maps[ch]++;
+
+    for (auto sim : maps)
+        cout << string(sim.second, sim.first);
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
---
+++ problems/9026/9026.cpp
@@ -0,0 +1,5 @@
+//https://www.e-olymp.com/ru/problems/9026
+void solve(int a)
+{
+    cout << a / 10 << " " << a % 10 << endl;
+}
