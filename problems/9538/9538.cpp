e195e212bc9d938e898efc25c464a1eab2ded142
Amir Farukshin <farukshin.amir@gmail.com>
2020-11-26 00:16:00 +0300

    docs(): зафиксировал решения задач 9538 9539

---
+++ problems/9538/9538.cpp
@@ -0,0 +1,32 @@
+//https://www.e-olymp.com/ru/problems/9538
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    // cout.setf(std::ios::fixed);
+    // cout.precision(4);
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << ((a * b) % 2 == 0 ? 0 : 1) << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
