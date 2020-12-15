cd9e3599eebf48b9662c406477627072302bfe0d
Amir Farukshin <farukshin.amir@gmail.com>
2020-12-15 21:48:00 +0300

    docs(): 7846

---
+++ problems/7846/7846.cpp
@@ -0,0 +1,40 @@
+//https://www.e-olymp.com/ru/problems/7846
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    // cout.setf(std::ios::fixed);
+    // cout.precision(2);
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    int m = -105, ind = -1;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        if (v[i] > m)
+            m = v[i], ind = i + 1;
+    }
+
+    cout << m << " " << ind << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
