902a6561f7664f927ce2bdf6b0f7266271d5603c
Amir Farukshin <farukshin.amir@gmail.com>
2020-12-24 23:16:47 +0300

    docs(): 63 8801 8802 8803

---
+++ problems/63/63.cpp
@@ -0,0 +1,32 @@
+//https://www.e-olymp.com/ru/problems/63
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+
+void solve()
+{
+    ll m, n;
+    cin >> m >> n;
+
+    ll ans = 1 + (n - 1) * (m - 1);
+    cout << ans << endl;
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while (t--) solve();
+
+    return 0;
+}
