6843816e2bdaec6ff04ffd3b61ca6ae7779acfc5
Amir Farukshin <farukshin.amir@gmail.com>
2020-12-28 20:08:53 +0300

    docs(): 2042 2043

---
+++ problems/2043/2043.cpp
@@ -0,0 +1,37 @@
+//https://www.e-olymp.com/ru/problems/2043
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    // cout.setf(std::ios::fixed);
+    // cout.precision(2);
+
+    int n;
+    cin >> n;
+    map<int, string> mp = {
+        {1, "January\nFebruary\nMarch"},
+        {2, "April\nMay\nJune"},
+        {3, "July\nAugust\nSeptember"},
+        {4, "October\nNovember\nDecember"}};
+
+    cout << mp[n] << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
