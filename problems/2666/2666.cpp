36ac59ea7bea807adf71164a2bee7e6b40d1e896
Amir Farukshin <farukshin.amir@gmail.com>
2021-01-08 22:14:25 +0300

    2666

---
+++ problems/2666/2666.cpp
@@ -0,0 +1,43 @@
+//https://www.e-olymp.com/ru/problems/2666
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    // cout.setf(std::ios::fixed);
+    // cout.precision(2);
+
+    ll n;
+    cin >> n;
+
+    for (int i = 0;i < n;i++)
+    {
+        for (int j = 0; j < n; j++)
+            if (j == n - i - 1)
+                cout << 0;
+            else if (j > n - i - 1)
+                cout << 1;
+            else
+                cout << 2;
+        cout << endl;
+    }
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
