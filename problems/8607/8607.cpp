d1db9bf6de4aba622f7b0ff9834f8e1034727bba
Amir Farukshin <farukshin.amir@gmail.com>
2021-01-26 22:48:30 +0300

    8607 8618

---
+++ problems/8607/8607.cpp
@@ -0,0 +1,37 @@
+//https://www.e-olymp.com/ru/problems/8607
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    // cout.setf(std::ios::fixed);
+    // cout.precision(1);
+
+    int n;
+    cin >> n;
+    ll ans = 0;
+    string s = to_string(n);
+    for (auto ch : s)
+        if (ch >= '0' && ch <= '9')
+            ans += (ch - '0') * (ch - '0');
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
