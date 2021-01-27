9e163a95f08def543bf5854d5980354aed7de3ef
Amir Farukshin <farukshin.amir@gmail.com>
2021-01-27 23:51:17 +0300

    docs(): зафиксировал скрипты для анализа задач

---
+++ countSolve.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+declare -A mp
+RESFILE="countSolve.txt"
+
+load(){	
+	page=($(echo $1))
+	link="https://www.e-olymp.com/ru/problems?page=$page"	
+	html=$(curl -s "$link")
+	array=($(echo $html \
+		| grep -Po '<a href="/ru/problems/([0-9]*)">[a-zA-Z0-9 а-яА-Я_ё\-\-?+*&*!#$@%()№:=]*</a></div><div class="eo-problem-row__complexity"><div>([0-9]*) / ([0-9]*)</div>' \
+		| sed -r '/^<a href="\/ru\/problems\/([0-9]*)">.*<\/a><\/div><div class="eo-problem-row__complexity"><div>([0-9]*) \/ ([0-9]*)<\/div>$/ s//\1 \2/' ))
+
+	for ((i=0; i<${#array[@]}; i+=2)); do
+	  echo "${array[i]} ${array[i+1]}" >> $RESFILE  
+	done
+}
+
+
+for ((j=0; j<308; j+=1)); do
+  echo "load page $j"
+  load $j  
+done
+
+exit 0
---
+++ diff.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+first=$1
+second=$2
+
+declare -A mp
+declare -A ans
+declare -A cnt
+
+array=($(cat countSolve.txt ))
+for ((i=0; i<${#array[@]}; i+=2)); do
+  cnt["${array[i]}"]=${array[i+1]}  
+done
+
+link="https://www.e-olymp.com/ru/users/$first/punchcard"
+html=$(curl -s "$link")
+array1=($(echo $html \
+	| grep -Po '<a title="([a-zA-Z0-9 а-яА-Я_ё\-\-?+*&*!#$@%()№:=]*), 100%" href="/ru/problems/[0-9]*" class='  \
+	| sed -r '/^<a title=".*, 100%" href="\/ru\/problems\/([0-9]*).*$/ s//\1/' \
+	| awk '{print $0;}' ))
+	
+link="https://www.e-olymp.com/ru/users/$second/punchcard"
+html=$(curl -s "$link")
+array2=($(echo $html \
+	| grep -Po '<a title="([a-zA-Z0-9 а-яА-Я_ё\-\-?+*&*!#$@%()№:=]*), 100%" href="/ru/problems/[0-9]*" class='  \
+	| sed -r '/^<a title=".*, 100%" href="\/ru\/problems\/([0-9]*).*$/ s//\1/' \
+	| awk '{print $0;}' ))	
+
+for item  in ${array2[*]}
+do
+	mp[$item]=1;
+done
+for item  in ${array1[*]}
+do
+	if [[ mp[$item] -eq 1 ]]
+	then
+		mp[$item]=0	
+	fi	
+done
+
+for key in "${!mp[@]}"; do
+    if [[ ${mp[$key]} -eq 1 ]]
+	then
+		ans[$key]=${cnt[$key]}		
+	fi
+done
+
+for key in "${!ans[@]}"; do
+    echo "https://www.e-olymp.com/ru/problems/${key} - ${ans[$key]}"
+done |
+sort -rn -k3
+
+echo "diff size = ${#ans[@]}"
+
+exit 0
