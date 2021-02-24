4cd646699af61cad8b19b6cd7d9722c1c1f1c1b4
Amir Farukshin <farukshin.amir@gmail.com>
2021-02-24 23:19:18 +0300

    перенес в архив

--- 8000-8999/8988/8988.cpp
+++ 8000-8999/8988/8988.cpp
@@ -1,48 +1,24 @@
 //https://www.e-olymp.com/ru/problems/8988
-//#tech_debt
-#include <bits/stdc++.h>
+#include<iostream>
+
+#include<string>
+
 using namespace std;
-
-typedef long long ll;
-typedef long double ld;
-
-void solve()
-{
-    string s;
-    getline(cin, s);
-
-    if (s.length() == 0)
-        return;
-    else if (s.length() == 1)
-    {
-        cout << s;
-        return;
-    }
-
-    for (int i = 0; i < s.length() - 1; i++)
-    {
-        if (s[i] == 'a' && s[i + 1] == 'b')
-        {
-            cout << "ups";
-            i += 1;
-        }
-        else
-            cout << s[i];
-    }
-    //cout << endl;
-}
 
 int main()
 {
-    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    string s;
+    getline(cin, s);
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] == 'a' && s[i + 1] == 'b')
+        {
+            s.erase(i, 2);
+            s.insert(i, "ups");
+            i++;
+        }
 
-#ifdef _DEBUG
-    freopen("input-1.txt", "r", stdin);
-    //freopen("output-1.txt", "w", stdout);
-#endif
-
-    solve();
-    //int t; cin >> t; while(t--) solve();
-
+    }
+    cout << s;
     return 0;
 }
